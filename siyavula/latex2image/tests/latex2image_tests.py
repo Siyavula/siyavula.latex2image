f6e4394198edabf4a9a2df54c90c75ef52fece42
HeatherW <heather@siyavula.com>
2016-10-03 11:35:24 +0200

    add more tests

--- siyavula/latex2image/tests/latex2image_tests.py
+++ siyavula/latex2image/tests/latex2image_tests.py
@@ -14,5 +14,5 @@ class TestBaseEquationToImageConversion(TestCase):
         self.div_input.set('class', 'latex-math')
 
     def test_complex_equation_to_png(self):
-        self.div_input.text = u'\\(\\begin{{aligned}} \\vec{{F}}_{{g}} & = m\\vec{{g}} \\\\ & = (\\text{{12,7}}\\ \\text{{kg}})(\\text{{9,8}}\\ \\text{{m·s$^{{-2}}$}}) \\\\ & = \\text{{124,46}}\\ \\text{{N}}\\text{{(&#181; µ &#956; μ}} \\end{{aligned}}\\)'.replace('{{', '{').replace('}}', '}')
+        self.div_input.text = u'\\(\\begin{{aligned}} \\vec{{F}}_{{g}} & = m\\vec{{g}} \\\\ & = (\\text{{12,7}}\\ \\text{{kg}})(\\text{{9,8}}\\ \\text{{m·s$^{{-2}}$}}) \\\\ & = \\text{{124,46}}\\ \\text{{N}}\\text{{&#181; µ &#956; μ}} &#181; µ &#956; μ \\end{{aligned}}\\'.replace('{{', '{').replace('}}', '}')
         self.assertEqual(replace_latex_with_images(self.element_input, 'latex-math', '', ''), None)
